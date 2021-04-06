f092c57ce26353eebc600cdbd78885254ce7c1da
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-04-06 18:32:12 +0300

    Clean up

--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	...require( '@wordpress/prettier-config' ),
-	printWidth: 140,
-	tabWidth: 4,
-};
--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => ( {
-	plugins: [
-		require( 'postcss-import' ),
-		require( 'postcss-selector-replace' )( {
-			before: [ '[prefix]' ],
-			after: [ 'sixa' ],
-		} ),
-	],
-} );
