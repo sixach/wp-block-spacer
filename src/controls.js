7929a72408a5792c0d293119504cabe1d5d58307
Markus <markus.goeckeritz@sixa.ch>
2021-07-16 17:41:22 +0200

    added utils and replaced control

--- src/controls.js
+++ src/controls.js
@@ -1,12 +1,7 @@
 /**
  * Utility for libraries from the `Lodash`.
  */
-import { merge } from 'lodash';
-
-/**
- * Utility helper methods specific for Sixa projects.
- */
-import { VisibilityToolbar } from '@sixa/wp-block-utils';
+import { merge, lowerCase } from 'lodash';
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -15,6 +10,21 @@ import { VisibilityToolbar } from '@sixa/wp-block-utils';
  * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
 import { BlockControls } from '@wordpress/block-editor';
+
+/**
+ * This packages includes a library of generic WordPress components to be used for
+ * creating common UI elements shared between screens and features of the WordPress dashboard.
+ *
+ * @see    https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
+ */
+import { ToolbarGroup, Dashicon } from '@wordpress/components';
+
+/**
+ * Retrieves the translation of text.
+ *
+ * @see    https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { sprintf } from '@wordpress/i18n';
 
 /**
  * The BlockToolbar component is used to render a toolbar that serves as a wrapper for number of options for each block.
@@ -27,13 +37,21 @@ import { BlockControls } from '@wordpress/block-editor';
  */
 export default function Controls( { attributes, setAttributes } ) {
 	const { visible } = attributes;
-
+	const handleOnChange = ( key, value ) => setAttributes( { visible: merge( {}, visible, { [ key ]: ! value } ) } );
 	return (
 		<>
 			<BlockControls group="block">
-				<VisibilityToolbar
-					devices={ visible }
-					onChange={ ( key, value ) => setAttributes( { visible: merge( {}, visible, { [ key ]: ! value } ) } ) }
+				<ToolbarGroup
+					controls={ map( visible, ( value, key ) => ( {
+						icon: <Dashicon icon={ key } />,
+						title: sprintf(
+							/* translators: %s: Device name. */
+							_x( 'Hide on %s?', 'visibility toolbar title', 'sixa' ),
+							lowerCase( get( attributes, `${ key }.label` ) )
+						),
+						onClick: () => handleOnChange( key, value ),
+						isActive: eq( ! value, false ),
+					} ) ) }
 				/>
 			</BlockControls>
 		</>
