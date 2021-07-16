ea758e8f2a5e40b441c6c3d970ded2238cb1412c
Markus <markus.goeckeritz@sixa.ch>
2021-07-16 18:00:20 +0200

    added namespace

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Sixa - Spacer
  * Description:     Spacer block for WordPress editor.
- * Version:         0.1.0
+ * Version:         1.0.0
  * Author:          sixa AG
  * License:         GPL-3.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
@@ -10,15 +10,20 @@
  *
  * @package         sixa
  */
+namespace SixaSpacerBlock;
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
  * through the block editor in the corresponding context.
  *
- * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
+ * @see       https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
+ * @since     1.0.0
+ * @return    void
  */
-function sixa_wp_block_spacer_init() {
+function register_block() {
 	register_block_type_from_metadata( __DIR__ );
 }
-add_action( 'init', 'sixa_wp_block_spacer_init' );
+add_action( 'init', __NAMESPACE__ . '\register_block' );
