3fb836664527758f232d4e0cb778203ab6ff296e
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-10-14 21:11:30 +0300

    Bootstrap the plugin

--- index.php
+++ index.php
@@ -3,26 +3,41 @@
  * Spacer Block.
  *
  * @wordpress-plugin
- * Plugin Name:          Sixa - Spacer
- * Description:          Spacer block for WordPress editor.
+ * Plugin Name:          Sixa - Spacer Block
+ * Description:          Block to add empty vertical space (full-width).
  * Version:              1.0.0
- * Requires at least:    5.8
- * Requires PHP:         7.4
+ * Requires at least:    5.7
+ * Requires PHP:         7.3
  * Author:               sixa AG
+ * Author URI:           https://sixa.ch
  * License:              GPL v3 or later
  * License URI:          https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:          sixa
+ * Text Domain:          sixa-block-spacer
  *
- * @package              sixa
+ * @package              Sixa_Blocks
  */
 
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
+/**
+ * Include the namespace of this block.
+ */
 use Sixa_Blocks\Spacer;
 
-// Composer autoload is needed in this package even if
-// it doesn't use any libraries to autoload the classes
-// from this package.
-require __DIR__ . '/vendor/autoload.php';
+/**
+ * Composer autoload is needed in this package even if
+ * it doesn't use any libraries to autoload the classes
+ * from this package.
+ *
+ * @see    https://getcomposer.org/doc/01-basic-usage.md#autoloading
+ */
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
+/**
+ * Initialize your block.
+ *
+ * Other than this function call, this file should not include any logic
+ * and should merely be used as an entry point to use this package as
+ * a WordPress plugin.
+ */
 Spacer::init();
