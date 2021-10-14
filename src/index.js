dfc98bb56f22d1746b900962c3de891e92c85aa2
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-10-15 00:44:46 +0300

    Imported block icon

--- src/index.js
+++ src/index.js
@@ -6,18 +6,10 @@
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * All files containing `style` keyword are bundled together. The code used
- * gets applied both to the front of your site and to the editor.
- *
- * @see    https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './style.scss';
-
-/**
  * Internal dependencies
  */
-import Edit from './edit';
+import Edit from './Edit';
+import Icon from './Icon';
 import save from './save';
 import transforms from './transforms';
 
@@ -33,17 +25,22 @@ registerBlockType( 'sixa/spacer', {
 	apiVersion: 2,
 
 	/**
-	 * @see    ./transforms.js
-	 */
-	transforms,
-
-	/**
-	 * @see    ./edit.js
+	 * @see    ./Edit.js
 	 */
 	edit: Edit,
+
+	/**
+	 * @see    ./Icon.js
+	 */
+	icon: Icon,
 
 	/**
 	 * @see    ./save.js
 	 */
 	save,
+
+	/**
+	 * @see    ./transforms.js
+	 */
+	transforms,
 } );
