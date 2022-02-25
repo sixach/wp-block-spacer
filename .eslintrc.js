593bac5fffd8b9fc4a830370918df4c76b912a2b
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-02-25 15:06:33 +0300

    Extend WordPress `eslint-plugin` config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	root: true,
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	rules: {
+		'react/jsx-key': 'off',
+		'jsdoc/check-line-alignment': 'off',
+		'no-nested-ternary': 'off',
+	},
+};
